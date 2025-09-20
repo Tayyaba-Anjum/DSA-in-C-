4792139889baa74423c44470c7970a98c227464a
Tayyaba-Anjum <tayybaanjum80@gmail.com>
2025-09-20 07:37:50 +0530

    Added marks

---
+++ array1/marks.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int marks[7];
+    for(int i=0;i<=6;i++){
+        cin>>marks[i];
+    }
+    for(int i=0;i<=6;i++){
+        if(marks[i]<35){
+            cout<<"Roll number whose marks are less than 35 is "<<i<<endl;
+        }
+    }
+}
